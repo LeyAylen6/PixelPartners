2c5d034b4bb84f8e61631290a0e2d9ac18397669
Ignacio <ignaciogimenez70@gmail.com>
2025-06-27 11:02:41 -0300

    feat: update db - add new classes

--- class/Project.php
+++ class/Project.php
@@ -1,38 +1,61 @@
 <?php
+require_once __DIR__ . '/User.php';
 
 class Project {
-  public $id;
-  public $name;
-  public $developer;
-  public $description;
-  public $image;
-  public $link;
+    public $id;
+    public $name;
+    public $developers = [];
+    public $description;
+    public $image;
+    public $link;
 
-  public function findAll($connection) {
-    $query = "SELECT * FROM `project`";
-    $projects = [];
+    public function findAll($connection) {
+        $query = "SELECT DISTINCT p.*, u.name as developer, u.image as user_image 
+                  FROM project p
+                  LEFT JOIN user_project up ON p.id = up.project_id
+                  LEFT JOIN user u ON up.user_id = u.id
+                  ORDER BY p.id, u.name";
+        
+        $projects = [];
+        $currentProjectId = null;
+        $currentProject = null;
 
-    try {
-      $response = mysqli_query($connection, $query);
+        try {
+            $response = mysqli_query($connection, $query);
 
-      while ($row = mysqli_fetch_assoc($response)) {
-          $project = new Project();
-          $project->id = $row["id"];
-          $project->name = $row["name"];
-          $project->developer = $row["developer"];
-          $project->description = $row["description"];
-          $project->image = $row["image"];
-          $project->link = $row["link"];
+            while ($row = mysqli_fetch_assoc($response)) {
+                if ($row["id"] !== $currentProjectId) {
+                    if ($currentProject !== null) {
+                        $projects[] = $currentProject;
+                    }
+                    
+                    $currentProject = new Project();
+                    $currentProject->id = $row["id"];
+                    $currentProject->name = $row["name"];
+                    $currentProject->description = $row["description"];
+                    $currentProject->image = $row["image"];
+                    $currentProject->link = $row["link"];
+                    $currentProjectId = $row["id"];
+                }
+                
+                if ($row["developer"] !== null && $row["user_image"] !== null) {
+                    $user = new User();
+                    $user->name = $row["developer"];
+                    $user->image = $row["user_image"];
+                    $currentProject->developers[] = $user;
+                }
+            }
 
-          $projects[] = $project;
-      }
+            if ($currentProject !== null) {
+                $projects[] = $currentProject;
+            }
 
-      return $projects;
-    
-    } catch (Exception $e) {
-      throw $e;
+            return $projects;
+        
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
-  }
 }
 
 ?> 
